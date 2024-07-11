c5452e584182adecaab55a74679389b6a504373f
Naim <nazrinaim@gmail.com>
2024-07-11 15:12:06 +0800

    Update api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\MemberController;
 
 Route::prefix('v1')-> group(function() {
 
@@ -12,6 +15,18 @@ Route::prefix('v1')-> group(function() {
 
 });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::group(['middleware' => 'auth:sanctum'], function(){
+    // Route::get("list/{id?}", [DeviceController::class, 'list']);
+    // Route::post("add", [DeviceController::class, 'add']);
+    // Route::put("update", [DeviceController::class, 'update']);
+    // Route::get("search/{name}", [DeviceController::class, 'search']);
+    // Route::delete("delete/{id}", [DeviceController::class, 'delete']);
+    // Route::post("save", [DeviceController::class, 'testData']);
+    // Route::resource("member", MemberController::class);
+//});
+
+//Route::post("login",[UserController::class,'index']);
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
