65f8d797db6b742082da95bcda2627efbc450ab8
Naim <nazrinaim@gmail.com>
2024-07-15 16:17:32 +0800

    remove unused controllers

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\DeviceController;
-use App\Http\Controllers\MemberController;
 
 Route::prefix('v1')-> group(function() {
 
@@ -14,19 +12,3 @@ Route::prefix('v1')-> group(function() {
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 
 });
-
-//Route::group(['middleware' => 'auth:sanctum'], function(){
-    // Route::get("list/{id?}", [DeviceController::class, 'list']);
-    // Route::post("add", [DeviceController::class, 'add']);
-    // Route::put("update", [DeviceController::class, 'update']);
-    // Route::get("search/{name}", [DeviceController::class, 'search']);
-    // Route::delete("delete/{id}", [DeviceController::class, 'delete']);
-    // Route::post("save", [DeviceController::class, 'testData']);
-    // Route::resource("member", MemberController::class);
-//});
-
-//Route::post("login",[UserController::class,'index']);
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
